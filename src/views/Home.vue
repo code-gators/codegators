9fffd07d8f4d9c6c5a1b839b6f1d1f8892d5966d
Alfredo Colon <acolon@hanekedesign.com>
2019-06-01 21:09:11 -0400

    Added card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="Home">
     <h1 class="Home__Title">Code Gators</h1>
+    <b-card
+      class="Home__Card"
+      :img-src="require('../assets/hand_code.png')"
+      img-alt="Card image"
+      img-right
+    >
+      <b-card-text>
+        Some quick example text to build on the card and make up the bulk of the
+        card's content.
+      </b-card-text>
+    </b-card>
   </div>
 </template>
 <style lang="scss">
 .Home {
   padding: 20px;
+  .card {
+    background: #00bc11;
+    color: #ffffff;
+
+    img {
+      height: 200px;
+    }
+  }
 }
 </style>
