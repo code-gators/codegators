1935539240b5ca1977c159cced4d0081fbff982b
Alfredo Colon <acolon@hanekedesign.com>
2019-06-03 16:17:06 -0400

    Profile route added

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,77 @@
 <template>
-  <div class="About">
+  <div class="Profile">
     <h1>Profile</h1>
     <h2>
       {{ `Ok ${user}, Let's start building your resume` }}
     </h2>
+    <div>
+      <b-card bg-variant="light" footer-tag="footer">
+        <b-form-group
+          label-cols-lg="3"
+          label="Basic information"
+          label-size="lg"
+          label-class="font-weight-bold pt-0"
+          class="mb-0"
+        >
+          <b-form-group
+            label-cols-sm="3"
+            label="Name:"
+            label-align-sm="right"
+            label-for="nested-street"
+          >
+            <b-form-input id="nested-street"></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            label-cols-sm="3"
+            label="Last name:"
+            label-align-sm="right"
+            label-for="nested-city"
+          >
+            <b-form-input id="nested-city"></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            label-cols-sm="3"
+            label="E-mail:"
+            label-align-sm="right"
+            label-for="nested-state"
+          >
+            <b-form-input id="nested-state"></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            label-cols-sm="3"
+            label="Address:"
+            label-align-sm="right"
+            label-for="nested-country"
+          >
+            <b-form-textarea
+              id="textarea"
+              placeholder="Enter something..."
+              rows="3"
+              max-rows="6"
+            ></b-form-textarea>
+          </b-form-group>
+
+          <!-- <b-form-group
+        label-cols-sm="3"
+        label="Ship via:"
+        label-align-sm="right" class="mb-0"
+      >
+        <b-form-radio-group
+          class="pt-2"
+          :options="['Air', 'Courier', 'Mail']"
+        ></b-form-radio-group>
+      </b-form-group> -->
+        </b-form-group>
+        <b-navbar slot="footer">
+          <b-nav-form class="ml-auto Header__Buttons">
+            <b-button variant="success">Continue</b-button>
+          </b-nav-form>
+        </b-navbar>
+      </b-card>
+    </div>
   </div>
 </template>
 <script>
@@ -18,7 +86,10 @@ export default {
 </script>
 
 <style lang="scss">
-.About {
+.Profile {
   padding: 20px;
+  .card-footer button {
+    margin-left: auto;
+  }
 }
 </style>
