8fc10ec85c820fa3db3b9a28e79100711185f188
Alfredo Colon <acolon@hanekedesign.com>
2019-06-04 13:46:04 -0400

    added lambdas

--- lambda/save_resume.js
+++ lambda/save_resume.js
@@ -25,9 +25,9 @@
 //   }
 // };
 
-exports.handler = function(event, context, callback) {
-  callback(null, {
+export async function handler(event, context) {
+  return {
     statusCode: 200,
-    body: "Hello, World"
-  });
+    body: JSON.stringify({ message: `Hello world ${Math.floor(Math.random() * 10)}` })
+  };
 }
