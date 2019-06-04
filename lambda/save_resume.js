6486af0c0d008ccf1afd2e007deef8fa7051c18e
Alfredo Colon <acolon@hanekedesign.com>
2019-06-04 12:49:25 -0400

    added lambdas

--- lambda/save_resume.js
+++ lambda/save_resume.js
@@ -1,19 +1,26 @@
-import axios from "axios";
-export async function handler(event, context) {
+import fetch from "node-fetch";
+
+exports.handler = async function(event, context) {
   try {
-    const response = await axios.get("https://icanhazdadjoke.com", {
-      headers: { Accept: "application/json" }
-    });
-    const data = response.data;
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText };
+    }
+    const data = await response.json();
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data.joke })
+      body: data.value
+      // if you want to return whole json string
+      // headers: { 'Content-Type': 'application/json' },
+      // body: JSON.stringify(data)
     };
   } catch (err) {
     console.log(err, event, context); // output to netlify function log
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+      body: err.message // Could be a custom message or object i.e. JSON.stringify(err)
     };
   }
-}
+};
